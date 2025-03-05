b89c05859b64e150ef3f9601da33212549d4c25b
kauriane1 <a.kalisa@alustudent.com>
2025-03-05 04:27:41 -0800

    My python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print()
-    return num
+"""Define a class Sqaure"""
+
+
+class Square:
+    """class sqaure"""
+    pass
