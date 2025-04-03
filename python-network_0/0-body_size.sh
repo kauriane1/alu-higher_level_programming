52b74e5ee240b238c7f67e6fe943e760c612adb3
kauriane1 <a.kalisa@alustudent.com>
2025-04-03 01:37:47 -0700

    My python network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Get the byte size of the HTTP response header for a given URL.
 curl -s "$1" | wc -c
-=======
-# Get the byte size of the HTTP response body for a given URL.
-curl -sL "$1" | wc -c
->>>>>>> e93b90a95770e0342452e1aaaa94dc34d76c6511
