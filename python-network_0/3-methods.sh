65ff683b7c6a8147415a2145226ce460412a8c01
kauriane1 <a.kalisa@alustudent.com>
2025-04-03 01:50:10 -0700

    Methods

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-<<<<<<< HEAD
+# Display all HTTP methods the server of a given URL will accept
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
-=======
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
->>>>>>> e93b90a95770e0342452e1aaaa94dc34d76c6511
