c7bc14f55b958f39c1cd2c5be1b2343714a9e724
kauriane1 <a.kalisa@alustudent.com>
2025-04-03 12:30:01 -0700

    Header

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-<<<<<<< HEAD
 curl -sH "X-School-User-Id: 98" "$1"
-=======
-curl -sH "X-School-User-Id: 98" "$1"
->>>>>>> e93b90a95770e0342452e1aaaa94dc34d76c6511
