df8e4f65cb139f7a606d196f870a4c2098e3b229
kauriane1 <a.kalisa@alustudent.com>
2025-04-03 01:47:20 -0700

    Deleting

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Send a DELETE request to a given URL and display the response body.
-<<<<<<< HEAD
 curl -sX DELETE "$1"
-=======
-curl -sX DELETE "$1"
->>>>>>> e93b90a95770e0342452e1aaaa94dc34d76c6511
