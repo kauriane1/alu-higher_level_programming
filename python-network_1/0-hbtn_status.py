6c69cc483d20e355b1dc92fa331ff755814cf575
kauriane1 <a.kalisa@alustudent.com>
2025-04-04 13:49:04 -0700

    Mean

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
 if url.startswith('https://'):
-   url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://alu-intranet.hbtn.io/status'
 if __name__ == '__main__':
     with urllib.request.urlopen(url) as f:
         content = f.read()
